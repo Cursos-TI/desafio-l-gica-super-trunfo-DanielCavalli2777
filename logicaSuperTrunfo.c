b90b60b58232413a84bab63b6848abca6e439a9f
DanielCavalli2777 <leaodrj@gmail.com>
2025-02-09 03:18:10 +0000

    colocado os dados da carta A e respectivo numeros dela

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -10,6 +10,7 @@ int main() {
 }
     // Definição das variáveis para armazenar as propriedades das cidades
     // Você pode utilizar o código do primeiro desafio
+    
     //São paulo corresponde a letra A
     //Minas Gerais corresponde a letra B
     //Rio Grande do sul corresponde a letra C
@@ -18,6 +19,47 @@ int main() {
     //Maranhão corresponde a letra F
     //Piauí corresponde a letra G
     //Amazonas corresponde a letra H
+
+    //São paulo Estados vai ser letra A
+
+    //cada numero vai corresponder uma cidade 
+    //cidade São Paulo vai ser 01
+    //cidade de Campinas vai ser 02
+    //cidade de Sumaré vai ser 03
+    //cidade de Hortolandia vai ser 04
+
+    //Estado: A
+    //Código da Carta: A01
+    //Nome da Cidade: São Paulo
+    //População: 12325000
+    //Área: 1521.11 km²
+    //PIB: 699.28 bilhões de reais
+    //Número de Pontos Turísticos: 50
+
+    //Estado: A
+    //Código da Carta: A02
+    //Nome da Cidade: Campinas
+    //População: 1223237	
+    //Área: 795.697 km²
+    //PIB: 46.68 bilhões de reais
+    //Número de Pontos Turísticos: 35
+
+    //Estado: A
+    //Código da Carta: A03
+    //Nome da Cidade: Sumaré
+    //População: 288245	
+    //Área:  153.470 km²
+    //PIB: 8.89 bilhões de reais
+    //Número de Pontos Turísticos: 20
+
+    //Estado: A
+    //Código da Carta: A04
+    //Nome da Cidade: Hortolândia
+    //População: 222186	
+    //Área:  62.420 km²
+    //PIB: 7.31 bilhões de reais
+    //Número de Pontos Turísticos: 15
+
 
     
     // Cadastro das Cartas:
